facfe9bcd1179c35f33df411400ff3ae514ee460
Raihan Tanvir <raihanntanvir@gmail.com>
2024-11-09 00:11:36 -0600

    Updated raspberryPi project name, fixed font sizing, updated intro card

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const fadeInElements = document.querySelectorAll('.fade-in');
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.1 });
+    fadeInElements.forEach(element => {
+        observer.observe(element);
+    });
+});
